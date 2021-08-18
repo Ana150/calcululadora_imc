a8b371b663d04fc48afd8d66ae511d2208bd3c18
Ana150 <anaoliveira4267@gmail.com>
2021-08-18 08:54:09 -0300

    JS iniciado

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const botao = document.getElementById("botao")
 const nome = document.getElementById("nome")
 const metros = document.getElementById("metros")
 const quilos = document.getElementById("quilos")
+const campo = document.getElementById("campo")
+
+
+botao.addEventListener("click", resultado)
+
+let resposta = "" 
+
+if (nome == "" | metros == "" | quilos == ""){
+
+}else{
+    const preResul = quilos / (metros * metros)
+}
+
+if(preResul < 18,5){
+    resposta = "Abaixo do peso"
+}else if (preResul >= 18,5 && preResul <= 24,9){
+    resposta = "Peso ideal"
+}else if (preResul >= 25,0 && preResul <= 29,9){
+    resposta = "Acima do peso"
+}else if (preResul >= 30,0 && preResul <= 34,9){
+    resposta = "Obesidade grau I"
+}else if (preResul >= 35,0 && preResul <= 39,9){
+    resposta = "Obesidade grau II"
+}else{
+    resposta = "Obesidade grau III"
+}
+
